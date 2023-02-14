e330f6d917068cdf55116e03aa2177d382459005
G-Geofrey <wanyamag17@gmail.com>
2023-02-15 00:39:00 +0200

    version='0.5.10'

--- ml/setup.py
+++ ml/setup.py
@@ -14,7 +14,7 @@ setup(
     # name that will be imported, can be different from code file name
     name='ml_processor',
 
-    version='0.5.9',
+    version='0.5.10',
 
     description='Includes functions for performing econometrics tasks',
 
