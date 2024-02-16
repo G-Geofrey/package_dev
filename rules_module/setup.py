8f7289e0fb3adcaa464b6c7fb2a61296a2725607
Geofrey Wanyama <geofrey.wanyama@monese.com>
2024-02-16 07:45:59 +0200

    initial commit of setup.py file

---
+++ rules_module/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import os
+
+setup(
+    name='rule_module',
+    version='0.0.1',
+    description='Functions for creating and anlysing rules',
+    packages=['rule_module'],
+    package_dir={'':'src'},
+    long_description='',
+    author='Geofrey Wanyama',
+    author_email='wanyamag17@gmail.com',
+    zip_safe=False,
+    classifers=[ 
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7", 
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+        ],
+
+)
